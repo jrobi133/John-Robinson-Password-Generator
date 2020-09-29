e98e7b382752f426ea0399d398ac40e4802e390c
John Robinson <johnarobinson133@gmail.com>
2020-09-29 15:39:56 -0600

    put in some characters

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Assignment Code
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase
 var generateBtn = document.querySelector("#generate");
 
+
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
