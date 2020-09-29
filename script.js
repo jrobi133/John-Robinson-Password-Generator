03e3b2ba5d034782c127bb51994c9e0f6231640f
John Robinson <johnarobinson133@gmail.com>
2020-09-29 15:49:06 -0600

    more variables made

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase
-var generateBtn = document.querySelector("#generate");
 
 
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+space = [];
+
+var choices;
+var toUpper = function (x) {
+  return x.toUpperCase();
+}
+alpha2 = alpha.map(toUpper);
+var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
